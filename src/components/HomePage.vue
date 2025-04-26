8c2a7bb955a4f65f92ca3c0bacb2844b435dc9b9
MessiahDev <alexmessias_18@yahoo.com.br>
2025-04-25 23:44:06 -0300

    Migração inicia

---
+++ src/components/HomePage.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home-page">
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomePage",
+  data() {
+    return {
+      message: "Hello, World!"
+    };
+  }
+};
+</script>
+
+<style scoped>
+.home-page {
+  text-align: center;
+  font-family: Arial, sans-serif;
+  color: #333;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="home">
+        <HelloWorld msg="Welcome to Financial Manager!" />
+    </div>
+</template>
+
+<script>
+import HelloWorld from "../components/HomePage.vue";
+
+export default {
+    name: "HomeView",
+    components: {
+        HelloWorld,
+    },
+};
+</script>
+
+<style scoped>
+.home {
+    text-align: center;
+    margin-top: 20px;
+}
+</style>
