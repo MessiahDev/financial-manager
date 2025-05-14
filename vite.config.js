a7553396b0c5a0374f94ed2162540b47754201c1
MessiahDev <alexmessias_18@yahoo.com.br>
2025-05-14 11:02:46 -0300

    fix: ajusta configuração do servidor para desabilitar HTTPS e corrigir o protocolo HMR

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import fs from 'fs'
 
 export default defineConfig({
   plugins: [vue()],
   server: {
     port: 5173,
     open: true,
-    https: process.env.NODE_ENV === 'production' ? false : {
-      key: fs.readFileSync('localhost-key.pem'),
-      cert: fs.readFileSync('localhost.pem'),
-    },
+    https: false,
     hmr: {
-      protocol: 'wss',
+      protocol: 'ws',
       host: 'localhost',
     }
   },
